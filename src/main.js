e128d3aebdc96248e56c0cd6e5bd708a97a3e9ad
六七 <806923284@qq.com>
2020-09-24 10:21:58 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//入口文件
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+new Vue({
+  el:"#app",
+  render:h=>h(App),
+  router
+})
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+//路由器对象模块
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import MSite from '../pages/MSite/MSite'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Search from '../pages/Search/Search'
+import Login from '../pages/Login/Login'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes:[
+    {
+      path:'/msite',
+      component:MSite,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path:'/order',
+      component:Order,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path:'/profile',
+      component:Profile,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path:'/search',
+      component:Search,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path:'/login',
+      component:Login
+    },
+    {
+      path: '/',
+      redirect:'/msite'
+    }
+  ]
+})
